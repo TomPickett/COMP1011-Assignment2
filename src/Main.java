b03ca6413689fcf87aa458b41e024497bfad3fb1
TomPickett <Pickett.tom7@gmail.com>
2015-02-05 20:09:54 -0500

    Finshed the super hero class

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
 
-		/**
-		 * @author Tom Pickett
-		 *Class Description: The main part of the project
-		 *@Version 0.4 - Added superhero Class
-		 */
+/**
+ * @author Tom Pickett
+ *Class Description: The main part of the project
+ *@Version 0.5 - finished superhero Class
+ */
 
-		public class Main {
+	public class Main {
 
-			public static void main(String[] args) {
+		public static void main(String[] args) {			
+			
+		//Instantiate Superhero
+		SuperHero superhero = new SuperHero("Spider Man");
+		superhero.showPowers();
+		superhero.fight();
 				
-				//Instantiate Superhero
-				SuperHero superhero = new SuperHero("Spider Man");
-				superhero.showPowers();
-				superhero.fight();
-				
-			}
 	}
+}
--- src/SuperHero.java
+++ src/SuperHero.java
@@ -1,27 +1,52 @@
 //Thomas Pickett
 
+import java.util.ArrayList;
+import java.util.*;
+
 public class SuperHero extends Hero {
 	
 	//Properties
-	private String superPowers;
-
+	private String[] superPowers = new String[3];
+	
 	
 	//Constructor
 	public SuperHero(String name) {
 		super(name);
 		
 		generateRandomPowers();
-		
-	}
+}
+
 
 	//Private Methods
-	private void generateRandomPowers() {
+
+	private void generateRandomPowers(){
 		
+		//List of powers		
+        ArrayList<String> powerList = new ArrayList<String>();
+		powerList.add("Super Speed");
+		powerList.add("Super Strength");
+		powerList.add("Body Armour");
+		powerList.add("Flight");
+		powerList.add("Fire Generation");
+		powerList.add("Weather Control");
+		
+		//Making the Powers Random 
+		Random power = new Random();
+		for(int i=0; i<3 ; i++){
+		int index = power.nextInt(powerList.size());
+		this.superPowers[i] = powerList.get(index);
+		 powerList.remove(index);
+		}
 	}
 	
 	
 	//Public Methods
-	public void showPowers() {
-		System.out.println(name + " super powers are: " + superPowers);
+	public void showPowers(){
+	    //Printing the output on screen for the user 
+		System.out.println(name + " powers are: ");
+		System.out.println("-----------------------------------------");
+		for(int i = 0; i < superPowers.length; i++){
+		System.out.println(superPowers[i]);}
+		System.out.println("-----------------------------------------");
 	}
 }
